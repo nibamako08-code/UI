47b4f14c47cb3e0cb4edcdb9c188004628e36e2a
nibamako08-code <nibamako08@gmail.com>
2026-06-04 05:47:28 +0700

    Update LOADER.lua

--- LOADER.lua
+++ LOADER.lua
@@ -36,6 +36,11 @@ if GameId == 1720936166 then
     NotifyLoad("All Star Tower Defense")
     loadstring(game:HttpGet("https://raw.githubusercontent.com/nibamako08-code/UI/refs/heads/main/PJ", true))()
 
+elseif GameId == 93978595733734 then
+    -- 2. Bizarre Lineage
+    NotifyLoad("Violent District")
+    loadstring(game:HttpGet("https://wareguardv2.xyz/raw_anyhub?hub=starhub&script=DV1"))()
+
 elseif GameId == 5130394318 then
     -- 2. Bizarre Lineage
     NotifyLoad("Bizarre Lineage")
